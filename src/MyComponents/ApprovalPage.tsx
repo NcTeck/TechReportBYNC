7fe42555205ce47649bc8dd66130ab03922aa252
NcTeck <ncteckllc@gmail.com>
2024-11-18 21:28:02 -0500

    m

--- src/MyComponents/ApprovalPage.tsx
+++ src/MyComponents/ApprovalPage.tsx
@@ -14,6 +14,11 @@ const auth = getAuth(app)
 const nameAdvRef = refDB(datab , "PreAprobal")
 const nameUserRef = refDB(datab , "Users")
 
+const [green , setgreen] =useState<string>()
+const [red , setred] =useState<string>('bg-red-400')
+const [yellow , setyellow] =useState<string>('bg-yellow-400')
+const [slate , setslate] =useState<string>('bg-slate-200')
+
 const [ros , setros] = useState<number[]>([]);
 const [namelogin , setnameogin] = useState<string>();
 const [visible , setvisible] = useState<any>();
@@ -21,13 +26,24 @@ const [visible , setvisible] = useState<any>();
 
 //Responses
 const [hvacR , setHvacR] = useState<any>("N/A");
+const [hvacFlat , setHvacFlat] = useState<number>(0);
 
+const [dashR , setDashR] = useState<any>("N/A");
+const [dashFlat , setdashFlat] = useState<number>(0);
 
-
+const [otherInR , setotherInR] = useState<any>("N/A");
+const [otherIntFlat , setotherIntFlat] = useState<number>(0);
 
 
 useEffect(()=>{
 
+    console.log(dashFlat + otherIntFlat);
+    
+
+    setgreen('bg-green-400')
+    setred('bg-red-400')
+    setyellow('bg-yellow-400')
+    setslate('bg-slate-400')
 
     onAuthStateChanged(auth , user =>{
 
@@ -82,7 +98,6 @@ const toggleVisibility =(itm:any)=>{
     if (itm != visible) {
          setvisible(itm)
     }else{setvisible(-1)}
-    // setVisibleItems((prev)=>prev.map((visible , i)=>(i=== itm ? !visible : visible)))
 
 
  }
@@ -118,28 +133,120 @@ onValue(refHvac , snap =>{
    hvac = snap.child("hvac").val()
    if (snap.child("status").val() == "ml") {
        hvacStatus = "Moderate";
-       hvacolor = "bg-yellow-400";
+       hvacolor = yellow;
    }
    if (snap.child("status").val() == "bad") {
-    hvacolor = "bg-red-400";
+    hvacolor = red;
     hvacStatus = snap.child("status").val()
 }
 if (snap.child("status").val() == "good") {
-    hvacolor = "bg-green-400";
+    hvacolor = green;
     hvacStatus = snap.child("status").val()
 
 }
 if (snap.child("status").val() == "No Selected") {
-    hvacolor = "bg-slate-100";
+    hvacolor = slate;
     hvacStatus = snap.child("status").val();
 
 }
-
-
-
    hvacUrls = snap.child("hvacurl").val()
    
 })
+
+//interior
+
+let interiorall:any;
+let interiorallStatus:any;
+let interiorallColor:any;
+console.log(interiorallColor);
+
+let dash:any;
+let dashStatus:any;
+let dashUrl:any;
+let dashColor:any;
+
+let otherinterior:any;
+let otherinteriorStatus:any;
+let otherInteriotUrl:any;
+let otherinteriorColor:any;
+
+
+const refInterior = refDB(datab , 'PreAprobal/'+item+"/"+"Interior")
+
+
+onValue(refInterior , snap =>{
+    
+
+   interiorall = snap.child("allLight").val()
+   if (snap.child("status").val() == "ml") {
+       interiorallStatus = "Moderate";
+       interiorallColor = yellow;
+
+   }
+   if (snap.child("status").val() == "bad") {
+    interiorallColor = red;
+    interiorallStatus = snap.child("status").val()
+}
+if (snap.child("status").val() == "good") {
+    interiorallColor = green;
+    interiorallStatus = snap.child("status").val()
+
+}
+if (snap.child("status").val() == "No Selected") {
+    interiorallColor = slate;
+    interiorallStatus = snap.child("status").val();
+
+}
+
+dash = snap.child("das").val()
+if (snap.child("dashstatus").val() == "ml") {
+    dashStatus = "Moderate";
+    dashColor = yellow;
+}
+if (snap.child("dashstatus").val() == "bad") {
+    dashColor = red;
+ dashStatus = snap.child("dashstatus").val()
+}
+if (snap.child("dashstatus").val() == "good") {
+    dashColor = green;
+ dashStatus = snap.child("dashstatus").val()
+
+}
+if (snap.child("status").val() == "No Selected") {
+    dashColor = slate;
+ dashStatus = snap.child("dashstatus").val();
+
+}
+dashUrl = snap.child("dashUrls").val()
+
+
+otherinterior = snap.child("interiorOther").val()
+if (snap.child("interiorOtherStatus").val() == "ml") {
+    otherinteriorStatus = "Moderate";
+    otherinteriorColor = yellow;
+}
+if (snap.child("interiorOtherStatus").val() == "bad") {
+    otherinteriorColor = red;
+    otherinteriorStatus = snap.child("interiorOtherStatus").val()
+}
+if (snap.child("interiorOtherStatus").val() == "good") {
+    otherinteriorColor = green;
+    otherinteriorStatus = snap.child("interiorOtherStatus").val()
+
+}
+if (snap.child("interiorOtherStatus").val() == "No Selected") {
+    otherinteriorColor = slate;
+    otherinteriorStatus = snap.child("interiorOtherStatus").val();
+
+}
+otherInteriotUrl = snap.child("interiorUrls").val()
+
+
+
+   
+})
+
+
 
 
 
@@ -149,44 +256,167 @@ if (snap.child("status").val() == "No Selected") {
             {visible == index  ? <div>
 
 
+
     
     <h3 className="text-gray-300 text-2xl">ROAD TEST</h3> <hr />
-<div> 
-Road Test? : {roadTest} <br />
-Stiker? : {stiker} <br />
+<table> 
+<tr>
+<th>Road Test :</th>
+<th> {roadTest} </th>
+</tr>
+
+<tr>
+    <th>Stiker</th>
+    <th>{stiker}</th>
+</tr>
+
     
- </div>
+ </table>
 
 
 
 
  <h3 className="text-gray-300 text-2xl">HVAC</h3> <hr />
-<div className={hvacolor}> 
- <div>
+<table> 
 
-    HVAC: {hvac} <br />
-    STATUS: {hvacStatus} <br />
+<tr>
+    <th>   HVAC</th>
+    <th>   {hvac}</th>
+</tr>
+<tr >
+    <th> STATUS</th>
+    <th className={hvacolor}>{hvacStatus}</th>
 
-    <img src={hvacUrls} width={50} className="cursor-pointer" onClick={()=> window.open(hvacUrls)} />
-
-    </div>
-
- { ( (hvacStatus == "good" || hvacStatus == "No Selected" )  ) ? null  : <div> 
+</tr>
+<tr>
+       <th >
+     <img src={hvacUrls} width={250} className="cursor-pointer" onClick={()=> window.open(hvacUrls)} />
+    </th> 
+    <th className={hvacolor}>
+         { ( (hvacStatus == "good" || hvacStatus == "No Selected" )  ) ? null  : <div> 
         
         Approve? <br />
       YES  <input type="radio" name="hvac" onChange={()=>setHvacR("Approved")}  />
        NO <input type="radio" name="hvac" onChange={()=>setHvacR("Declined")}  />
+       <br />
+       {hvacR == "Approved" ? <div> Flat Rate : <input type="number" onChange={(e)=>setHvacFlat(parseFloat(e.target.value))} /> </div> : null }
 
          </div>  }
+    </th>
+</tr>
+
+
     
- </div>
+ </table>
+
+<br />
+<hr />
+<br />
 
 
+<h3 className="text-gray-300 text-2xl">Interior</h3> <hr />
+<table> 
+
+<tr>
+    <th>   All Interior</th>
+    <th>   {interiorall}</th>
+</tr>
+<tr >
+    <th> Status</th>
+    <th className={hvacolor}>{interiorallStatus}</th>
+</tr>
+
+
+{/* Dash */}
+
+<tr>
+    <br />
+</tr>
+
+<tr>
+    <th>Dash</th>
+    <th>   {dash}</th>
+</tr>
+<tr >
+    <th> STATUS</th>
+    <th className={dashColor}>{dashStatus}</th>
+
+</tr>
+<tr>
+       <th >
+     <img src={dashUrl} width={250} className="cursor-pointer" onClick={()=> window.open(dashUrl)} />
+    </th> 
+    <th className={dashColor}>
+         { ( (dashStatus == "good" || dashStatus == "No Selected" )  ) ? null  : <div> 
+        
+        Approve? <br />
+      YES  <input type="radio" name="dass" onChange={()=>setDashR("Approved")}  />
+       NO <input type="radio" name="dass" onChange={()=>setDashR("Declined")}  />
+       <br />
+       {dashR == "Approved" ? <div> Flat Rate : <input type="number" onChange={(e)=>setdashFlat(parseFloat(e.target.value))} /> </div> : null }
+
+         </div>  }
+    </th>
+</tr>
+
+
+{/* Other interior */}
+<tr>
+    <br />
+</tr>
+
+<tr>
+    <th>   Other Issue Internal</th>
+    <th>   {otherinterior}</th>
+</tr>
+<tr >
+    <th> STATUS</th>
+    <th className={otherinteriorColor}>{otherinteriorStatus}</th>
+
+</tr>
+<tr>
+       <th >
+     <img src={otherInteriotUrl} width={250} className="cursor-pointer" onClick={()=> window.open(otherInteriotUrl)} />
+    </th> 
+    <th className={otherinteriorColor}>
+         { ( (otherinteriorStatus == "good" || otherinteriorStatus == "No Selected" )  ) ? null  : <div> 
+        
+        Approve? <br />
+      YES  <input type="radio" name="otint" onChange={()=>setotherInR("Approved")}  />
+       NO <input type="radio" name="otint" onChange={()=>setotherInR("Declined")}  />
+       <br />
+       {otherInR == "Approved" ? <div> Flat Rate : <input type="number" onChange={(e)=>setotherIntFlat(parseFloat(e.target.value))} /> </div> : null }
+
+         </div>  }
+    </th>
+</tr>
+
+
+
+
+
+    
+ </table>
+
+<br />
+<hr />
+<br />
+
+
+
+
+
+
+
+
+<br />
+<br />
+<br />
 
 
 
 <button onClick={()=>{
-alert(item + " - " + hvacR)
+alert(item + " - " + hvacR + " flat is "+ hvacFlat )
 window.location.reload()
 }}>Ver</button>
 </div> : null }
